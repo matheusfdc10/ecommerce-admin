c36bba3b8ffaef84f41d2e88570b339afd44dfc3
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2023-07-19 01:33:49 -0300

    up

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,6 +3,7 @@ export default function AuthLayout({
 }: {
     children: React.ReactNode
 }) {
+    
     return (
         <div className="flex items-center justify-center h-full">
             {children}
